c67223699695a658a5a30cc5cf7d687aef19db8a
Kochela <josephngalu96@gmail.com>
2026-03-24 13:00:04 +0300

    updated db url

--- prisma/migrations/20260324082332_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Job" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "budget" REAL NOT NULL,
-    "location" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
